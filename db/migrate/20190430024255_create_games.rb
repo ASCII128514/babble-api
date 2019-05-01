580f26e6c0e8e327a1b2f18f007cd8a6d359fee4
Paul Lu <luhanfeng1106@sina.com>
2019-05-01 16:11:52 +0800

    bug fix

--- db/migrate/20190430024255_create_games.rb
+++ db/migrate/20190430024255_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.integer :rounds
-      t.integer :find_partner_timer
+      t.integer :find_parter_timer
       t.integer :selfie_timer
       t.integer :question_timer
       t.references :user, foreign_key: true
