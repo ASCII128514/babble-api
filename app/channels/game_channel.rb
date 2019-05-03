49f71f4b6f52952501724ae0707e88f05b1dd558
Paul Lu <luhanfeng1106@sina.com>
2019-05-03 14:37:06 +0800

    add unsubscribe

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -8,8 +8,6 @@ class GameChannel < ApplicationCable::Channel
     # para = JSON.parse(params)
     token = params[:token]
     openid = decode(token)
-    p openid
-    puts "run!!\n\n\n\n"
     @user = User.find_by(openid: openid[:token])
     # find the game and add a join table between the user and the game
     @game = Game.find(params[:room])
@@ -18,7 +16,21 @@ class GameChannel < ApplicationCable::Channel
     @subscriber.game = @game
     @subscriber.user = @user
     @subscriber.save
-
+    ids = []
+    @users = []
+    u = @game.users
+    puts "run!!\n\n\n\n"
+    # filter the people that is not in the room
+    u.each do |user|
+      puts user.id
+      puts 'inside loop'
+      unless ids.include?(user.id)
+        ids << user.id
+        @users << user
+      end
+    end
+    p ids
+    p @users
     stream_from "game_channel_#{params[:room]}"
     # GameChannel.broadcast_to(
     #   "game_channel_#{params[:room]}",
@@ -30,11 +42,20 @@ class GameChannel < ApplicationCable::Channel
 
     ActionCable.server.broadcast("game_channel_#{params[:room]}",
                                  type: 'users',
-                                 players: @game.users)
+                                 players: @users)
   end
 
   def unsubscribed
     # remove the subscriber join table if the user close the connection
+    p params[:token]
+    puts "in unsubscribe\n\n\n\n\n\n\n\n"
+    token = params[:token]
+    openid = decode(token)
+    @user = User.find_by(openid: openid[:token])
+    sub = @user.subscribers.find { |x| x.game.id == params[:room] }
+    p sub
+    puts "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n"
+    sub.destroy
   end
 
   private
